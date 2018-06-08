ce7c3971363920625705edbb9b772a7b08467237
Jelle Damen <info@jelledamen.nl>
2018-06-08 13:56:24 +0200

    BS4 Media Object
    Updated to BS4 Media Object

--- bootstrap4-comment-walker.php
+++ bootstrap4-comment-walker.php
@@ -15,7 +15,7 @@ namespace App;
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-class Bootstrap_Comment_Walker extends \Walker_Comment
+class bootstrap_comment_walker extends \Walker_Comment
 {
     /**
      * Output a comment in the HTML5 format.
@@ -35,36 +35,26 @@ class Bootstrap_Comment_Walker extends \Walker_Comment
         <<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $this->has_children ? 'parent media' : 'media' ); ?>>
 
         <?php if ( 0 != $args['avatar_size'] ): ?>
-            <div class="media-left">
-                <a href="<?php echo get_comment_author_url(); ?>" class="media-object">
-                    <?php echo get_avatar( $comment, $args['avatar_size'] ); ?>
-                </a>
-            </div>
+            <a href="<?php echo get_comment_author_url(); ?>" class="media-avatar">
+                <?php echo get_avatar( $comment, $args['avatar_size'] ); ?>
+            </a>
         <?php endif; ?>
 
         <div class="media-body">
-
             <?php printf( '<h4 class="media-heading">%s</h4>', get_comment_author_link() ); ?>
-
-            <div class="comment-metadata">
-                <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID, $args ) ); ?>">
+                <a class="comment-metadata" href="<?php echo esc_url( get_comment_link( $comment->comment_ID, $args ) ); ?>">
                     <time datetime="<?php comment_time( 'c' ); ?>">
                         <?php printf( _x( '%1$s at %2$s', '1: date, 2: time' ), get_comment_date(), get_comment_time() ); ?>
                     </time>
-                </a>
-            </div><!-- .comment-metadata -->
-
+                </a><!-- .comment-metadata -->
             <?php if ( '0' == $comment->comment_approved ) : ?>
                 <p class="comment-awaiting-moderation label label-info"><?php _e( 'Your comment is awaiting moderation.' ); ?></p>
             <?php endif; ?>
-
             <div class="comment-content">
                 <?php comment_text(); ?>
             </div><!-- .comment-content -->
-
             <ul class="list-inline">
                 <?php edit_comment_link( __( 'Edit' ), '<li class="edit-link">', '</li>' ); ?>
-
                 <?php
                 comment_reply_link( array_merge( $args, array(
                     'add_below' => 'div-comment',
@@ -74,9 +64,7 @@ class Bootstrap_Comment_Walker extends \Walker_Comment
                     'after'     => '</li>'
                 ) ) );
                 ?>
-
             </ul>
-
         </div>
         <?php
     }
